5e9d992d9dd4e884a39863c3f4d193be6e5a1082
szxuhongye <hongye.xu@rutgers.edu>
2020-06-01 19:03:06 -0400

    url config setting

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,4 +1,4 @@
-"""Ins_Demo URL Configuration
+"""Insta URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from Insta.views import HellowWorld
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('insta/', include("Insta.urls"))
+    path('', HellowWorld.as_view(), name='helloworld')
 ]
---
+++ Insta/views.py
@@ -0,0 +1,5 @@
+from django.views.generic import TemplateView
+
+class HellowWorld(TemplateView):
+    template_name = 'test.html'
+
